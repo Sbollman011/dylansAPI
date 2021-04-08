ec97396a91737f07cf41eda1a0c95e93724d0f33
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 14:16:24 -0700

    body parser

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let locationR ="lo";
 
 app.get('/', function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+   res.set('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
     var homepageText = {
       text: textR,
       location: locationR
@@ -24,10 +24,25 @@ app.get('/', function(req, res){
   });
 
   app.post('/',(req, res) => {
-    textR= req.body.user;
+    var textR= req.body.user;
     locationR = req.body.password;
     console.log("User name = "+user_name+", password is "+password);
     res.end("yes");
   });
 
+  app.get('/api/users', function(req, res) {
+    var user_id = req.param('id');
+    var token = req.param('token');
+    var geo = req.param('geo');
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});
+
+app.post('/api/users', function(req, res) {
+  var user_id = req.body.id;
+  var token = req.body.token;
+  var geo = req.body.geo;
+
+  res.send(user_id + ' ' + token + ' ' + geo);
+});
   app.listen((process.env.PORT || 5000));
