61180b497425bf2eff074714bad64d18206549b3
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 13:18:12 -0700

    body parser

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const $ = require('cheerio');
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const bodyParser = require('body-parser');
+
 
 const app  = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
