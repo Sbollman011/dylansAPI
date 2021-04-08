ed51890bd6ae03456ecef933194a098a20195aa8
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 12:34:29 -0700

    Fxed port

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app  = express();
 
 app.get('/', function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
-    var homepageText ="<p1 style = 'font-size: 62px;' >Welcome to Rapid Stock Checkers API. <p1> <br/><br/><br/> <p style='color: #2699FB; text-decoration: underline; font-size: 32;'> Buy Now pages url: https://www.retailapiservice.com/buylinks <br/><br/>"+ 
-    "Best Buy API url: https://www.retailapiservice.com/bestbuy<p> <p style='color: #2699FB; text-decoration: underline; font-size: 32;'> Database url: https://www.retailapiservice.com/productlist <br/><br/>"
+    var homepageText = {
+      text: "hello"
+    } 
     res.send(homepageText);
   });
 
