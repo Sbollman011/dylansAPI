30cda2d682ed943b14f89073c6981b6263b1d21a
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 14:21:20 -0700

    body parser

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ app.get('/', function(req, res){
     res.end("yes");
   });
 
-  app.get('/api/users', function(req, res) {
+  /*app.get('/api/users', function(req, res) {
     var user_id = req.param('id');
     var token = req.param('token');
     var geo = req.param('geo');
 
     res.send(user_id + ' ' + token + ' ' + geo);
-});
+});*/
 
 app.post('/api/users', function(req, res) {
   var user_id = req.body.id;
