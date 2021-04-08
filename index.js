1dc661b143a138b83f284feb74312e16382c42a9
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 12:19:00 -0700

    lol

---
+++ index.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const $ = require('cheerio');
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
+const app  = express();
+
+app.get('/', function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var homepageText ="<p1 style = 'font-size: 62px;' >Welcome to Rapid Stock Checkers API. <p1> <br/><br/><br/> <p style='color: #2699FB; text-decoration: underline; font-size: 32;'> Buy Now pages url: https://www.retailapiservice.com/buylinks <br/><br/>"+ 
+    "Best Buy API url: https://www.retailapiservice.com/bestbuy<p> <p style='color: #2699FB; text-decoration: underline; font-size: 32;'> Database url: https://www.retailapiservice.com/productlist <br/><br/>"
+    res.send(homepageText);
+  });
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+  })
