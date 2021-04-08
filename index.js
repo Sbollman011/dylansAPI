98de12f21a5e58f3aa3c0882ec34a9f26ef1d561
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-04-08 14:32:06 -0700

    body parser

--- index.js
+++ index.js
@@ -10,44 +10,25 @@ const app  = express();
 
 app.use(express.json());
 
-var user_id = '';
-var token = '';
-var geo = '';
+var text= '';
+var location1 = '';
+var location2 = '';
 
 app.get('/', function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
    res.set('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
     var homepageText = {
-      text: "plase",
-      location: "please"
+      text: text,
+      location1: location1,
+      location2: location2
     } 
     res.send(homepageText);
   });
 
   app.post('/',(req, res) => {
-    var textR= req.body.user;
-    locationR = req.body.password;
-    console.log("User name = "+user_name+", password is "+password);
-    res.end("yes");
-  });
+    text = req.body.text;
+    location1 = req.body.location1,
+    location2 = req.body.location2
+   });
 
-  app.get('/api/users', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-   res.set('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-    var homepageText = {
-      user_id: user_id,
-      token: token,
-      geo: geo
-      
-    } 
-    res.send(homepageText);
-  });
-
-app.post('/api/users', function(req, res) {
-   user_id = req.body.id;
-   token = req.body.token;
-   geo = req.body.geo;
-
-  res.send(user_id + ' ' + token + ' ' + geo);
-});
   app.listen((process.env.PORT || 5000));
